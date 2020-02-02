dbbf49bb81a914bda1bf0cb50304bdb8b8901188
Cybernuki <1164@holbertonschool.com>
2020-02-02 15:26:06 -0500

    add target folder of testing

---
+++ answers/0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ *  bubble_sort - sort an array with bubble sort
+ *  Each swap prints the array
+ *  @array: is the given array
+ *  @size: is the size of the aray
+ *  Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ answers/1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ *  insertion_sort - sorts a list using selection sort
+ *  each swamp prints the list
+ *  @list: is the given list
+ *  Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
